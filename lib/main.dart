fbe3febeee56797dfbca73e9b7e30e7629c49eea
Rethek <Rethek.kumar@yahoo.com>
2024-05-28 20:52:55 -0400

    Added firebase configuration for android application

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:bargainbites/app.dart';
 import 'package:bargainbites/utils/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const App());
 
   // Todo: Add widgets binding
