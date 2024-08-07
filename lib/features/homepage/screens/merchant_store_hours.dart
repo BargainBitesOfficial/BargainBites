9d99624cc8629e368ced2cdad31fe870724629aa
Mohit Gohil <gohilmohit933@gmail.com>
2024-08-07 03:11:56 -0400

    update the code with DB storing

--- lib/features/homepage/screens/merchant_store_hours.dart
+++ lib/features/homepage/screens/merchant_store_hours.dart
@@ -5,7 +5,7 @@ import '../../../utils/constants/text_styles.dart';
 
 class StoreHoursPage extends StatefulWidget {
   final String merchantId;
-  final String merchantName;// Pass the merchant ID to this page
+  final String merchantName;
 
   StoreHoursPage({required this.merchantId, required this.merchantName});
 
@@ -15,35 +15,50 @@ class StoreHoursPage extends StatefulWidget {
 
 class _StoreHoursPageState extends State<StoreHoursPage> {
   final _formKey = GlobalKey<FormState>();
+  bool isLoading = true; // Flag to indicate if data is being loaded
+  bool isError = false; // Flag to indicate if an error occurred
 
-  Map<String, StoreHour> storeHours = {
-    'Monday': StoreHour(day: 'Mon', isOpen: false, openTime: '00:00 AM', closeTime: '00:00 PM'),
-    'Tuesday': StoreHour(day: 'Tues', isOpen: false, openTime: '00:00 AM', closeTime: '00:00 PM'),
-    'Wednesday': StoreHour(day: 'Wed', isOpen: false, openTime: '00:00 AM', closeTime: '00:00 PM'),
-    'Thursday': StoreHour(day: 'Thur', isOpen: false, openTime: '00:00 AM', closeTime: '00:00 PM'),
-    'Friday': StoreHour(day: 'Fri', isOpen: false, openTime: '00:00 AM', closeTime: '00:00 PM'),
-    'Saturday': StoreHour(day: 'Sat', isOpen: false, openTime: '00:00 AM', closeTime: '00:00 PM'),
-    'Sunday': StoreHour(day: 'Sun', isOpen: false, openTime: '00:00 AM', closeTime: '00:00 PM'),
+  Map<String, StoreTiming> storeHours = {
+    'Monday': StoreTiming(day: 'Monday', isOpen: false, openTime: '00:00 AM', closeTime: '00:00 PM'),
+    'Tuesday': StoreTiming(day: 'Tuesday', isOpen: false, openTime: '00:00 AM', closeTime: '00:00 PM'),
+    'Wednesday': StoreTiming(day: 'Wednesday', isOpen: false, openTime: '00:00 AM', closeTime: '00:00 PM'),
+    'Thursday': StoreTiming(day: 'Thursday', isOpen: false, openTime: '00:00 AM', closeTime: '00:00 PM'),
+    'Friday': StoreTiming(day: 'Friday', isOpen: false, openTime: '00:00 AM', closeTime: '00:00 PM'),
+    'Saturday': StoreTiming(day: 'Saturday', isOpen: false, openTime: '00:00 AM', closeTime: '00:00 PM'),
+    'Sunday': StoreTiming(day: 'Sunday', isOpen: false, openTime: '00:00 AM', closeTime: '00:00 PM'),
   };
 
   @override
   void initState() {
     super.initState();
-    _loadStoreHours();
+    _fetchStoreHours();
   }
 
-  Future<void> _loadStoreHours() async {
-    DocumentSnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore.instance
-        .collection('Merchants')
-        .doc(widget.merchantId)
-        .get();
+  Future<void> _fetchStoreHours() async {
+    try {
+      DocumentSnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore.instance
+          .collection('Merchants')
+          .doc(widget.merchantId)
+          .get();
 
-    if (snapshot.exists && snapshot.data()!.containsKey('storeTiming')) {
-      setState(() {
-        storeHours = (snapshot.data()!['storeTiming'] as Map<String, dynamic>).map((key, value) {
+      if (snapshot.exists && snapshot.data()!.containsKey('storeTiming')) {
+        Map<String, StoreTiming> fetchedHours = (snapshot.data()!['storeTiming'] as Map<String, dynamic>).map((key, value) {
           var v = Map<String, String>.from(value);
-          return MapEntry(key, StoreHour.fromMap(v));
+          return MapEntry(key, StoreTiming.fromMap(v));
         });
+        setState(() {
+          storeHours = fetchedHours;
+          isLoading = false; // Data loaded successfully
+        });
+      } else {
+        setState(() {
+          isLoading = false; // Data loaded but storeTiming key not found
+        });
+      }
+    } catch (e) {
+      setState(() {
+        isError = true;
+        isLoading = false; // Error occurred during data fetching
       });
     }
   }
@@ -60,11 +75,54 @@ class _StoreHoursPageState extends State<StoreHoursPage> {
       });
 
       print("Store hours updated successfully!");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Store hours updated successfully', style: TextStyle(fontFamily: "Poppins")),
+          backgroundColor: TColors.primary,
+        ),
+      );
+    }
+  }
+
+  Future<void> _selectTime(BuildContext context, TextEditingController controller) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay(
+        hour: int.parse(controller.text.split(":")[0]),
+        minute: int.parse(controller.text.split(":")[1].split(" ")[0]),
+      ),
+    );
+    if (picked != null) {
+      final localizations = MaterialLocalizations.of(context);
+      final formattedTime = localizations.formatTimeOfDay(picked, alwaysUse24HourFormat: false);
+      setState(() {
+        controller.text = formattedTime;
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    if (isLoading) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: TColors.primary,
+          title: Text(widget.merchantName, style: TextStyles.regulartext(color: TColors.bWhite)),
+        ),
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+
+    if (isError) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: TColors.primary,
+          title: Text(widget.merchantName, style: TextStyles.regulartext(color: TColors.bWhite)),
+        ),
+        body: Center(child: Text('Error: Failed to load store hours')),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: TColors.primary,
@@ -119,8 +177,8 @@ class _StoreHoursPageState extends State<StoreHoursPage> {
 }
 
 class StoreHourTile extends StatefulWidget {
-  final StoreHour storeHour;
-  final ValueChanged<StoreHour> onChanged;
+  final StoreTiming storeHour;
+  final ValueChanged<StoreTiming> onChanged;
 
   StoreHourTile({required this.storeHour, required this.onChanged});
 
@@ -139,6 +197,36 @@ class _StoreHourTileState extends State<StoreHourTile> {
     isOpen = widget.storeHour.isOpen;
     openTimeController = TextEditingController(text: widget.storeHour.openTime);
     closeTimeController = TextEditingController(text: widget.storeHour.closeTime);
+  }
+
+  @override
+  void dispose() {
+    openTimeController.dispose();
+    closeTimeController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _selectTime(BuildContext context, TextEditingController controller) async {
+    if (isOpen) {
+      final TimeOfDay? picked = await showTimePicker(
+        context: context,
+        initialTime: TimeOfDay(
+          hour: int.parse(controller.text.split(":")[0]),
+          minute: int.parse(controller.text.split(":")[1].split(" ")[0]),
+        ),
+      );
+      if (picked != null) {
+        final localizations = MaterialLocalizations.of(context);
+        final formattedTime = localizations.formatTimeOfDay(picked, alwaysUse24HourFormat: false);
+        setState(() {
+          controller.text = formattedTime;
+          widget.onChanged(widget.storeHour.copyWith(
+            openTime: controller == openTimeController ? formattedTime : widget.storeHour.openTime,
+            closeTime: controller == closeTimeController ? formattedTime : widget.storeHour.closeTime,
+          ));
+        });
+      }
+    }
   }
 
   @override
@@ -174,14 +262,19 @@ class _StoreHourTileState extends State<StoreHourTile> {
                 ),
               ),
               readOnly: !isOpen,
+              onTap: () {
+                if (isOpen) _selectTime(context, openTimeController);
+              },
               validator: (value) {
-                if (value == null || value.isEmpty) {
+                if (isOpen && (value == null || value.isEmpty)) {
                   return 'Required';
                 }
                 return null;
               },
               onSaved: (value) {
-                widget.onChanged(widget.storeHour.copyWith(openTime: value));
+                if (value != null) {
+                  widget.onChanged(widget.storeHour.copyWith(openTime: value));
+                }
               },
             ),
           ),
@@ -200,14 +293,19 @@ class _StoreHourTileState extends State<StoreHourTile> {
                 ),
               ),
               readOnly: !isOpen,
+              onTap: () {
+                if (isOpen) _selectTime(context, closeTimeController);
+              },
               validator: (value) {
-                if (value == null || value.isEmpty) {
+                if (isOpen && (value == null || value.isEmpty)) {
                   return 'Required';
                 }
                 return null;
               },
               onSaved: (value) {
-                widget.onChanged(widget.storeHour.copyWith(closeTime: value));
+                if (value != null) {
+                  widget.onChanged(widget.storeHour.copyWith(closeTime: value));
+                }
               },
             ),
           ),
@@ -217,21 +315,21 @@ class _StoreHourTileState extends State<StoreHourTile> {
   }
 }
 
-class StoreHour {
+class StoreTiming {
   final String day;
   final bool isOpen;
   final String? openTime;
   final String? closeTime;
 
-  StoreHour({required this.day, required this.isOpen, this.openTime, this.closeTime});
+  StoreTiming({required this.day, required this.isOpen, this.openTime, this.closeTime});
 
-  StoreHour copyWith({
+  StoreTiming copyWith({
     String? day,
     bool? isOpen,
     String? openTime,
     String? closeTime,
   }) {
-    return StoreHour(
+    return StoreTiming(
       day: day ?? this.day,
       isOpen: isOpen ?? this.isOpen,
       openTime: openTime ?? this.openTime,
@@ -248,8 +346,8 @@ class StoreHour {
     };
   }
 
-  factory StoreHour.fromMap(Map<String, String> map) {
-    return StoreHour(
+  factory StoreTiming.fromMap(Map<String, String> map) {
+    return StoreTiming(
       day: map['day']!,
       isOpen: map['isOpen'] == 'true',
       openTime: map['openTime'],
